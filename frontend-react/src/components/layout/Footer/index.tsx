e848d5feb6ce96fbf5b295d50e86bd6e78b0aa46
Adam Hemzal <adahemzal@gmail.com>
2023-02-07 08:00:50 -0800

    added Header and Footer components to the layout

---
+++ frontend-react/src/components/layout/Footer/index.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className='w-full py-6 flex items-center justify-center mt-auto' data-testid="footer">
+      <p className="text-xs m-0">@2023 Adam Hemzal</p>
+    </footer>
+  )
+}
+export default Footer;
---
+++ frontend-react/src/components/layout/Header/index.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header className="">
+      <h1>CZK Currency Convertor</h1>
+    </header>
+  );
+};
+export default Header;
