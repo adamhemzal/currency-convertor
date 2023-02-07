cffc9f848b6430719d49986aaba7f0508727c339
Adam Hemzal <adahemzal@gmail.com>
2023-02-07 10:16:31 -0800

    refactor table component

--- frontend-react/src/components/table/ExchangeRatesTable/index.tsx
+++ frontend-react/src/components/table/ExchangeRatesTable/index.tsx
@@ -1,34 +1,53 @@
-type Props = {
-  data: any
-}
+import type { ExchangeRates } from "@ts/types";
 
-const ExchangeRatesTable = ({data}: Props) => {
-  
-  return(
-    <div className="overflow-x-auto py-4">
-      <table className="min-w-full divide-y-2 divide-gray-200 text-sm">
-        <thead>
-          <tr>
-            {Object.keys(data.rates[0]).map( (key: string) => (
-              <th key={key} className="whitespace-nowrap px-4 py-2 text-left font-medium">
-                {key.toUpperCase()}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {data.rates.map( (rate: any) => (
-            <tr key={rate.code}>
-              <td className="whitespace-nowrap px-4 py-2">{rate.country}</td>
-              <td className="whitespace-nowrap px-4 py-2">{rate.currency}</td>
-              <td className="whitespace-nowrap px-4 py-2">{rate.amount}</td>
-              <td className="whitespace-nowrap px-4 py-2">{rate.code}</td>
-              <td className="whitespace-nowrap px-4 py-2">{rate.rate}</td>
+type Props = {
+  rates: ExchangeRates[];
+  date: string;
+};
+
+const ExchangeRatesTable = ({ rates, date }: Props) => {
+  return (
+    <div className="mb-10">
+      <div className="flex items-center justify-between flex-row">
+        <p className="bloc text-xs">
+          Last update: <span className="font-bold">{date}</span>
+        </p>
+        <p className="block text-xs">
+          Data are provided by{" "}
+          <a href="https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt">
+            CNB
+          </a>
+        </p>
+      </div>
+
+      <div className="overflow-x-auto rounded-md border">
+        <table className="min-w-full divide-y-2 divide-zinc-300 text-sm table-auto">
+          <thead>
+            <tr>
+              {Object.keys(rates[0]).map((key: string) => (
+                <th
+                  key={key}
+                  className="whitespace-nowrap px-4 py-3 text-left bg-zinc-100 font-bold"
+                >
+                  {key.toUpperCase()}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="divide-y divide-zinc-200">
+            {rates.map((rate: any) => (
+              <tr key={rate.code} className="odd:bg-zinc-50">
+                <td className="whitespace-nowrap px-4 py-2">{rate.country}</td>
+                <td className="whitespace-nowrap px-4 py-2">{rate.currency}</td>
+                <td className="whitespace-nowrap px-4 py-2">{rate.amount}</td>
+                <td className="whitespace-nowrap px-4 py-2">{rate.code}</td>
+                <td className="whitespace-nowrap px-4 py-2">{rate.rate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 export default ExchangeRatesTable;
--- frontend-react/src/pages/home.tsx
+++ frontend-react/src/pages/home.tsx
@@ -1,22 +1,49 @@
-import { useEffect, useState } from "react"
+import { useQuery } from "@tanstack/react-query";
+import { getExchangeRates } from "@api/api";
+import Loader from "@components/utils/Loader";
+import ExchangeRatesTable from "@components/table/ExchangeRatesTable";
 
-export const Home = () => {
-  const [rates, setRates] = useState<any>();
+const Home = () => {
+  const { data, error, isLoading } = useQuery({queryKey: ['rates'], queryFn: getExchangeRates});
+
+  const handleConversion = (e: any) => {
+    e.preventDefault();
+    console.log(e.target)
+  }
   
-  const fetchData = async (): Promise<void> => {
-    const res = await fetch("http://localhost:8080/");
-    const data = await res.json();
-
-    console.log({data});
+  if(isLoading) { 
+    return <Loader />
   }
 
-  useEffect( () => {
-    fetchData()
-  }, [])
+  if(error) {
+    return <p>An error has occurred</p>
+  } 
 
   return(
     <>
-      <h1>Home page</h1>
+      <form onSubmit={handleConversion} className="border">
+        <label htmlFor="amountBeforeId">You have CZK</label>
+        <input type="number" name="amountBefore" id="amountBeforeId" />
+
+        <select name="currencySelect">
+          <option defaultValue="">Select currency</option>
+          {data.rates.map( (rate: any) => {
+            console.log(rate.rate);
+            return(
+              <option key={rate.code} value={rate.code}>{rate.code}</option>
+            )
+          })}
+        </select>
+
+        <label htmlFor="amountAfterId">You get</label>
+        <output name="convertedAmount" htmlFor="amountBeforeId"></output>
+
+        <button type="submit">Convert</button>
+      </form>
+
+      <h2 className="text-h3">Current exchange rates</h2>
+      <ExchangeRatesTable date={data.date} rates={data.rates} />
     </>
   )
 }
+export default Home;
