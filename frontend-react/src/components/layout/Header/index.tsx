a1686c9a2ed83217b009f396fc9483fe331b61cc
Adam Hemzal <adahemzal@gmail.com>
2023-02-07 15:59:05 -0800

    removed uppercase from header title and changed text

--- frontend-react/src/components/layout/Header/index.tsx
+++ frontend-react/src/components/layout/Header/index.tsx
@@ -1,7 +1,11 @@
+import { RiCoinLine } from "react-icons/ri";
+
 const Header = () => {
   return (
-    <header className="">
-      <h1>CZK Currency Convertor</h1>
+    <header className="container mt-6" data-testid="header">
+      <h1 className="m-0 text-center">
+        <span className="flex justify-center items-center"><RiCoinLine size="3rem" /></span>
+        Currency Convertor</h1>
     </header>
   );
 };
